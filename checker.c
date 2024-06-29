d49066cac0261dbb2d850be33bc30b0b7947b33b
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-06-29 11:23:55 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,49 +1,19 @@
 #include <stdio.h>
 #include <assert.h>
 
-int isInRange(float value, float min, float max) {
-    return (value >= min && value <= max);
-}
+int isInRange(float Value, float Min, float Max){
+	return (Value >= Min && Value <= Max);
 
-void printAbnormalMeasure(const char* measure, int isHigh) {
-    printf("Abnormal %s: %s\n", measure, isHigh ? "High" : "Low");
-}
+int batteryIsOk(float temperature, float soc, float chargeRate){
+	!isInRange(temperature, 0, 45) && printf("Temperature out of range!\n");
+	!isInRange(soc, 20, 80) && printf("State of charge out of range!\n");
+	chargeRate > 0.8 && printf("Charge Rate out of range!\n");
 
-int validateTemperature(float temperature) {
-    if (!isInRange(temperature, 0, 45)) {
-        printf("Temperature out of range!\n");
-        printAbnormalMeasure("Temperature", temperature > 45);
-        return 0;
-    }
-    return 1;
-}
+	return isInRange(temperature, 0, 45) && isInRange(soc, 20,80) && chargeRate <= 0.8 && printf("battery in good condition\n");
 
-int validateSoc(float soc) {
-    if (!isInRange(soc, 20, 80)) {
-        printf("State of Charge out of range!\n");
-        printAbnormalMeasure("SOC", soc > 80);
-        return 0;
-    }
-    return 1;
-}
 
-int validateChargeRate(float chargeRate) {
-    if (chargeRate > 0.8) {
-        printf("Charge Rate out of range!\n");
-        printAbnormalMeasure("Charge Rate", 1);
-        return 0;
-    }
-    return 1;
-}
-
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-    return validateTemperature(temperature) &&
-           validateSoc(soc) &&
-           validateChargeRate(chargeRate);
-}
-
-int main() {
-    assert(batteryIsOk(25, 70, 0.7));
-    assert(!batteryIsOk(50, 85, 0));
-    return 0;
+int main()
+{
+  assert(batteryIsOk(25, 70, 0.7));
+  assert(!batteryIsOk(50, 85, 0));
 }
