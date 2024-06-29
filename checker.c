cfe3a47bdf811582320d9414f8444e9f27c1d54b
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-06-29 08:31:41 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -10,11 +10,11 @@ int batteryIsOk(float temperature, float soc, float chargeRate){
 	!isInRange(soc, 20, 80) && printf("State of charge out of range!\n");
 	chargeRate > 0.8 && printf("Charge Rate out of range!\n");
 	
-	return isInRange(temperature, 0, 45) && isInRange(soc, 20,80) && chargeRate <= 0.8;
+	return isInRange(temperature, 0, 45) && isInRange(soc, 20,80) && chargeRate <= 0.8 && printf("Battery in Good condition :)\n");
 }
 
 int main()
 {
-  //assert(batteryIsOk(25, 70, 0.7));
+  assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
