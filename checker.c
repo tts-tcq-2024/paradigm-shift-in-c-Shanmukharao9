4ad8af0f46d25e4245310540d60d6a19702e75dc
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-06-29 08:24:06 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -15,6 +15,6 @@ int batteryIsOk(float temperature, float soc, float chargeRate){
 
 int main()
 {
-  assert(batteryIsOk(25, 70, 0.7));
+  //assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
