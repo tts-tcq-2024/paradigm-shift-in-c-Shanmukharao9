03dfd40fdcb78b6f11109287c12c75912cd736b6
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-18 11:22:20 +0530

    Create testcases.c

---
+++ testcases.c
@@ -0,0 +1,43 @@
+#include <assert.h>
+#include "Battery_Monitor.h"
+#include "Testcases.h"
+
+void testTemperatureBelowLowerLimit() {
+    assert(!batteryIsOk(-2, 45, 0.8));
+}
+
+void testTemperatureAboveUpperLimit() {
+    assert(!batteryIsOk(46, 45, 0.8));
+}
+
+void testSocBelowLowerLimit() {
+    assert(!batteryIsOk(50, 19, 0.4));
+}
+
+void testSocAboveUpperLimit() {
+    assert(!batteryIsOk(20, 81, 0.4));
+}
+
+void testChargeRateAboveUpperLimit() {
+    assert(!batteryIsOk(30, 75, 0.9));
+}
+
+void testApproachingLowerTemperatureLimit() {
+    assert(batteryIsOk(2.25, 75, 0.6));
+}
+
+void testApproachingHigherTemperatureLimit() {
+    assert(batteryIsOk(42.75, 75, 0.6));
+}
+
+void testApproachingDischarge() {
+    assert(batteryIsOk(20, 24, 0.6));
+}
+
+void testApproachingChargePeak() {
+    assert(batteryIsOk(20, 76, 0.6));
+}
+
+void testApproachingMaxChargeRate() {
+    assert(batteryIsOk(20, 40, 0.76));
+}
